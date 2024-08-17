bdf8e7836c78a6298025d3a732e1202c978a5983
FiLKoNd <fil.yt.pass@gmail.com>
2024-08-18 01:06:36 +0300

    fix command

--- upgrades-example/src/main/java/com/filkond/upgrades_example/command/UpgradeExecutor.java
+++ upgrades-example/src/main/java/com/filkond/upgrades_example/command/UpgradeExecutor.java
@@ -4,6 +4,7 @@ import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import com.filkond.upgrades.configuration.SimpleUpgradeType;
+import com.filkond.upgrades.configuration.UpgradeType;
 import com.filkond.upgrades_example.PlayerUpgradesController;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -21,7 +22,7 @@ public class UpgradeExecutor extends BaseCommand {
     @Syntax("<player> <upgrade-type> <upgrade-level>")
     public void set(Player sender,
                     @Name("target") OnlinePlayer target,
-                    @Name("upgrade-type") SimpleUpgradeType upgradeType,
+                    @Name("upgrade-type") UpgradeType upgradeType,
                     @Name("upgrade-level") Integer level) {
         controller.getHolderOrDefault(target.getPlayer()).setLevel(upgradeType, level);
         sender.sendMessage("Set " + target.getPlayer().getName() + " " + upgradeType.getId() + " to level " + level);
@@ -30,7 +31,7 @@ public class UpgradeExecutor extends BaseCommand {
     @Subcommand("get")
     @Syntax("<player> <upgrade-type>")
     @CommandCompletion("@players @types")
-    public void get(CommandSender sender, @Flags("other") Player target, SimpleUpgradeType type) {
+    public void get(CommandSender sender, @Flags("other") Player target, UpgradeType type) {
         int level = controller.getHolderOrDefault(target).getLevel(type);
         sender.sendMessage("Level of %s %s is %d".formatted(target.getName(), type.getId(), level));
     }
